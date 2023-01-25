f6014fc559d267f1b862cab7c9ec3b7228a861f3
Reza Kalbali <rezaka1001@gmail.com>
2023-01-25 12:11:28 +0330

    Add outside click handler for user avatar

--- src/app/components/header/avatar/avatar.component.ts
+++ src/app/components/header/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
@@ -10,5 +10,12 @@ export class AvatarComponent {
 
   public onClickAvatar(): void {
     this.avatarDisplay = this.avatarDisplay === 'none' ? 'block' : 'none';
+  }
+  @ViewChild('avatar') public avatar!: ElementRef<HTMLDivElement>;
+  @HostListener('document:click', ['$event'])
+  public clickOut(event: any): void {
+    if (!this.avatar.nativeElement.contains(event.target)) {
+      this.avatarDisplay = 'none';
+    }
   }
 }
