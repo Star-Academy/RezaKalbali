f5dbcd920e4706cc49f96899d287d8207a86ef30
Reza Kalbali <rezaka1001@gmail.com>
2023-01-26 14:27:55 +0330

    Use webp & svg images

--- src/app/demo-data/slide-list.ts
+++ src/app/demo-data/slide-list.ts
@@ -1,6 +1,6 @@
 export default [
-  { src: 'assets/images/slideshow/cp.jpg', alt: 'cyber punk' },
-  { src: 'assets/images/slideshow/csgo.jpg', alt: 'counter strike GO' },
-  { src: 'assets/images/slideshow/dota2.jpg', alt: 'dota 2' },
-  { src: 'assets/images/slideshow/warzone.jpg', alt: 'war zone 2' },
+  { src: 'assets/images/slideshow/cp.webp', alt: 'cyber punk' },
+  { src: 'assets/images/slideshow/csgo.webp', alt: 'counter strike GO' },
+  { src: 'assets/images/slideshow/dota2.webp', alt: 'dota 2' },
+  { src: 'assets/images/slideshow/warzone.webp', alt: 'war zone 2' },
 ];
