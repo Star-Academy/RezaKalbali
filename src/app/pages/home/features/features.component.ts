f9cb9d836f70aa92661b710a50ed0ec0f17bd474
Reza Kalbali <rezaka1001@gmail.com>
2023-02-16 09:37:27 +0330

    Solve auth & features PR comments

--- src/app/pages/home/features/features.component.ts
+++ src/app/pages/home/features/features.component.ts
@@ -1,12 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.scss'],
 })
-export class FeaturesComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class FeaturesComponent {}
