c2a608462a45a9af74beced1255718ba7b855c8a
Reza Kalbali <rezaka1001@gmail.com>
2023-02-19 12:25:14 +0330

    Add auth component test

--- src/app/pages/auth/auth.component.spec.ts
+++ src/app/pages/auth/auth.component.spec.ts
@@ -24,4 +24,58 @@ describe('AuthComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should login mode default value true', function () {
+        expect(component.isLoginMode).toBeTrue();
+    });
+
+    it('should have invalid status for form', function () {
+        expect(component.isSubmitted).toBeFalse();
+        component.formSubmitHandler();
+        fixture.detectChanges();
+
+        expect(component.isSubmitted).toBeTrue();
+        expect(component.userForm.valid).toBeFalse();
+    });
+
+    it('should call loginHandler', function () {
+        spyOn(component, 'loginHandler');
+        spyOn(component, 'registerHandler');
+
+        component.formSubmitHandler();
+        expect(component.isSubmitted).toBeTrue();
+        expect(component.loginHandler).toHaveBeenCalled();
+        expect(component.registerHandler).not.toHaveBeenCalled();
+    });
+
+    it('should not call registerHandler', function () {
+        component.isLoginMode = false;
+        fixture.detectChanges();
+        spyOn(component, 'loginHandler');
+        spyOn(component, 'registerHandler');
+
+        component.formSubmitHandler();
+        expect(component.isSubmitted).toBeTrue();
+        expect(component.registerHandler).not.toHaveBeenCalled();
+        expect(component.loginHandler).not.toHaveBeenCalled();
+    });
+
+    it('should call registerHandler', function () {
+        component.isLoginMode = false;
+        component.userForm.setValue({
+            username: 'rezakalbali',
+            password: '12345678',
+            email: 'rezaka1001@gmail.com',
+            repeatPassword: '12345678',
+        });
+        fixture.detectChanges();
+
+        spyOn(component, 'loginHandler');
+        spyOn(component, 'registerHandler');
+
+        component.formSubmitHandler();
+        expect(component.isSubmitted).toBeTrue();
+        expect(component.registerHandler).toHaveBeenCalled();
+        expect(component.loginHandler).not.toHaveBeenCalled();
+    });
 });
