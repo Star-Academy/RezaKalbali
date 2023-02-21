6894aa301b0911a8cbb90d43651a8cb805918ede
Reza Kalbali <rezaka1001@gmail.com>
2023-02-21 09:06:08 +0330

    Add search navigate to header search-box

--- src/app/components/header/search-box/search-box.component.ts
+++ src/app/components/header/search-box/search-box.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-search-box',
     templateUrl: './search-box.component.html',
     styleUrls: ['./search-box.component.scss'],
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+    public searchTerm: string = '';
+
+    constructor(private router: Router) {}
+
+    public async searchSubmitHandler(): Promise<void> {
+        await this.router.navigate(['/search'], {queryParams: {search_term: this.searchTerm, order: 'popular'}});
+    }
+}
